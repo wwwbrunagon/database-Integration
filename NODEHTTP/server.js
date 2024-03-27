b4dc0d8929ab2f32e5abf1ac074d8b969fdb4230
Bruna Gonçalves <wwwbrunagon@Brunas-MacBook-Pro-2.local>
2024-03-27 20:39:54 -0300

    http methods & RESTful APis

---
+++ NODEHTTP/server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  const { headers, method, url } = req;
+  console.log(headers, method, url);
+  res.end('Hello World');
+});
+
+const PORT = 8000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
