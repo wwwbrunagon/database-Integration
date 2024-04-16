6073d000e0209bdaa24707bb65bc0a9f1059add2
wwwbrunagon <wwwbrunagon@gmail.com>
2024-04-15 22:38:24 -0300

    middleware

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const morgan = require('morgan');
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
@@ -8,6 +10,11 @@ const bootcamps = require('./routes/bootcamps');
 dotenv.config({ path: './config/config.env' });
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'dev') {
+  app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
